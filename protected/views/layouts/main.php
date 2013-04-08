eed326bd2e3f9dd948648d2d238a535dfb30c58d
Darren <darren.chee@pentajeu.com>
2013-04-08 18:58:42 +0800

    initial commit

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,70 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/12.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/text.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container_16">
+
+	<div class="row grid_16" id="line">
+		<div>
+			<h1 class="logo" style="margin-bottom: 0px;"><a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logo.png" alt=""></a></h1>
+		</div>
+
+		<div id="mainmenu">
+			<?php $this->widget('zii.widgets.CMenu',array(
+				'items'=>array(
+					array('label'=>'Home', 'url'=>array('/site/index')),
+					array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+					array('label'=>'Contact', 'url'=>array('/site/contact')),
+					array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				),
+			)); ?>
+		</div><!-- mainmenu -->
+	</div>
+
+	<div class="clear"></div>
+
+	<!-- <div id="mainmenu" class="grid_16">
+		<?php if(isset($this->breadcrumbs)):?>
+			<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+				'links'=>$this->breadcrumbs,
+			)); ?>
+		<?php endif?>
+	</div> -->
+
+	<div class="clear"></div>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+	<div id="footer" class="grid_16">
+		Copyright &copy; <?php echo date('Y'); ?> by GroupM.<br/>
+		All Rights Reserved.
+	</div><!-- footer -->
+
+	<div class="clear"></div>
+</div><!-- page -->
+
+</body>
+</html>
---
+++ protected/views/site/index.php
@@ -0,0 +1,30 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<div class="grid_16">
+	<h1>Lorem Ipsum</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non blandit dui. 
+		Phasellus dolor erat, condimentum eget suscipit nec, sodales auctor purus. 
+		Maecenas vel odio auctor quam dictum egestas non sit amet tortor. Donec vehicula, mauris et 
+		porta pellentesque, libero orci ultrices lorem, sed consequat lorem magna non dui. 
+		Nullam posuere eros id turpis hendrerit venenatis. Curabitur a libero a metus pulvinar 
+		fringilla vitae tincidunt enim. Nulla facilisi. Nullam eget orci risus, ut eleifend lacus. 
+		Maecenas tristique elit vel ligula euismod sit amet porta enim varius. Vivamus in justo lectus. 
+		Suspendisse tortor tellus, sodales at accumsan sit amet, molestie sodales augue. Integer convallis 
+		turpis nec augue rutrum laoreet. Phasellus non nisi id felis ultricies pellentesque et id libero.
+	</p>
+
+	<p>
+		Nullam eu turpis id felis semper elementum eleifend ut nibh. Etiam porttitor urna sit amet 
+		sapien pulvinar non porttitor sem elementum. Nam egestas luctus aliquam. Duis id sapien ac elit 
+		placerat pharetra. Sed vulputate nibh at dui venenatis non tincidunt ligula fermentum. Phasellus 
+		vehicula libero gravida metus euismod fringilla. Etiam eu lacus ut lacus eleifend lobortis quis tempus 
+		sapien. Curabitur et pellentesque elit. Fusce at diam orci, ut mattis mi. Aliquam lacinia felis vel 
+		dolor bibendum laoreet ornare tortor luctus.
+	</p>
+</div>
