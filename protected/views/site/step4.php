dc7a8061d2ccd0959439ba4d82c5a5ab29b10022
lcc91 <leechechian@gmail.com>
2013-05-29 16:39:08 +0800

    update the last step

--- protected/views/site/step4.php
+++ protected/views/site/step4.php
@@ -2,6 +2,10 @@
 $cs = Yii::app()->getClientScript();
 $cs->registerPackage('form-wizard');
 $cs->registerPackage('tag-it');
+$cs->registerPackage('organic-tab');
+$cs->registerScript('tab', "
+	$('#example-two').organicTabs();
+	");
 $cs->registerScript('tag-it', "
 	$('#tags').tagit();
 	");
@@ -18,30 +22,42 @@ $cs->registerCss('tag-it', "
 	}
 	");
 ?>
-<div class="container">
+<div class="grid_16">
 	<h2>Congratulation! <?php echo $model->name; ?> created.</h2>
-	<div class="span-12">
-		<h2 style="margin-bottom:0;"><?php echo $model->form; ?></h2>
-		<form style='background:#eee'>
-			<?php echo $model->wizard; ?>
-		</form>
-	</div>
+    <div id="example-two">
+            <ul class="nav">
+                    <li class="nav-one"><a href="#summary" class="current">Summary</a></li>
+                    <li class="nav-two"><a href="#html">HTML Code</a></li>
+            </ul>
+            <div class="list-wrap">
+                    <div id="summary">
+                        <div class="container">
+                            <div class="span-12">
+                                    <h2 style="margin-bottom:0;"><?php echo $model->form; ?></h2>
+                                    <form style='background:#eee'>
+                                            <?php echo $model->wizard; ?>
+                                    </form>
+                            </div>
 
-	<div class="span-12 wizard last">
-		<h2 style="margin-bottom:0;">Tags</h2>
-		<ul id="tags">
-		<?php foreach ($model->tags as $tag): ?>
-			<li><?php echo $tag; ?></li>
-		<?php endforeach; ?>
-		</ul>
-	</div>
+                            <div class="span-12 wizard last">
+                                    <h2 style="margin-bottom:0;">Tags</h2>
+                                    <ul id="tags">
+                                    <?php foreach ($model->tags as $tag): ?>
+                                            <li><?php echo $tag; ?></li>
+                                    <?php endforeach; ?>
+                                    </ul>
+                            </div>
 
-	<div class="buttons">
-		<a href="<?php echo $this->createUrl('site/campaign'); ?>" class="next">All Campaign</a>
-                <a href="<?php echo $this->createUrl("site/result/{$model->eventId}"); ?>" class="next">Report</a>
-	</div>
-
-	<div class="row">
-		<iframe src="<?php echo $this->createUrl('formgenerator', array('id'=>$model->eventId)); ?>" style="width: 100%; height: 200px;"></iframe>
-	</div>
+                            <div class="buttons">
+                                    <a href="<?php echo $this->createUrl('site/campaign'); ?>" class="next">All Campaign</a>
+                                    <a href="<?php echo $this->createUrl("site/result/{$model->eventId}"); ?>" class="next">Report</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="html" class="hide">
+                        <label>Embed code to your website:</label><br/>
+                        <textarea id="holdtext" style="width: 50%; height: 100px;"><iframe src="<?php echo $this->createAbsoluteUrl('formgenerator', array('id'=>$model->eventId)); ?>" style="width: 100%; height: 200px;"></iframe></textarea><br/>
+                    </div>
+            </div>
+    </div>
 </div>
