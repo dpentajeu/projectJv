cc7a4cf9f7cb483aa5c13a12f0beae4b5b2cf89e
Jay Lim <jay@pentajeu.com>
2013-06-03 00:11:48 +0800

    Show the full html form output

--- protected/views/site/step4.php
+++ protected/views/site/step4.php
@@ -1,5 +1,6 @@
 <?php
 $cs = Yii::app()->getClientScript();
+$baseUrl = Yii::app()->request->baseUrl;
 $cs->registerPackage('form-wizard');
 $cs->registerPackage('tag-it');
 $cs->registerPackage('organic-tab');
@@ -8,6 +9,12 @@ $cs->registerScript('tab', "
 	");
 $cs->registerScript('tag-it', "
 	$('#tags').tagit();
+	");
+$cs->registerScript('iframe', "
+	var url = '{$this->createUrl('formgenerator', array('id'=>$model->eventId))}';
+	$.get(url, function(result) {
+		$('#holdtext').text(result);
+	});
 	");
 $cs->registerCss('tag-it', "
 	form .row input {
@@ -24,40 +31,38 @@ $cs->registerCss('tag-it', "
 ?>
 <div class="grid_16">
 	<h2>Congratulation! <?php echo $model->name; ?> created.</h2>
-    <div id="example-two">
-            <ul class="nav">
-                    <li class="nav-one"><a href="#summary" class="current">Summary</a></li>
-                    <li class="nav-two"><a href="#html">HTML Code</a></li>
-            </ul>
-            <div class="list-wrap">
-                    <div id="summary">
-                        <div class="container">
-                            <div class="span-12">
-                                    <h2 style="margin-bottom:0;"><?php echo $model->form; ?></h2>
-                                    <form style='background:#eee'>
-                                            <?php echo $model->wizard; ?>
-                                    </form>
-                            </div>
+	<div id="example-two">
+		<ul class="nav">
+			<li class="nav-one"><a href="#summary" class="current">Summary</a></li>
+			<li class="nav-two"><a href="#html">HTML Code</a></li>
+		</ul>
+		<div class="list-wrap">
+			<div id="summary">
+				<div class="container">
+					<div class="span-12">
+						<h2 style="margin-bottom:0;"><?php echo $model->form; ?></h2>
+						<form style='background:#eee'><?php echo $model->wizard; ?></form>
+					</div>
 
-                            <div class="span-12 wizard last">
-                                    <h2 style="margin-bottom:0;">Tags</h2>
-                                    <ul id="tags">
-                                    <?php foreach ($model->tags as $tag): ?>
-                                            <li><?php echo $tag; ?></li>
-                                    <?php endforeach; ?>
-                                    </ul>
-                            </div>
+					<div class="span-12 wizard last">
+						<h2 style="margin-bottom:0;">Tags</h2>
+						<ul id="tags">
+						<?php foreach ($model->tags as $tag): ?>
+							<li><?php echo $tag; ?></li>
+						<?php endforeach; ?>
+						</ul>
+					</div>
 
-                            <div class="buttons">
-                                    <a href="<?php echo $this->createUrl('site/campaign'); ?>" class="next">All Campaign</a>
-                                    <a href="<?php echo $this->createUrl("site/result/{$model->eventId}"); ?>" class="next">Report</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="html" class="hide">
-                        <label>Embed code to your website:</label><br/>
-                        <textarea id="holdtext" style="width: 50%; height: 100px;"><iframe src="<?php echo $this->createAbsoluteUrl('formgenerator', array('id'=>$model->eventId)); ?>" style="width: 100%; height: 200px;"></iframe></textarea><br/>
-                    </div>
-            </div>
-    </div>
-</div>
+					<div class="buttons">
+						<a href="<?php echo $this->createUrl('site/campaign'); ?>" class="next">All Campaign</a>
+						<a href="<?php echo $this->createUrl("site/result/{$model->eventId}"); ?>" class="next">Report</a>
+					</div>
+				</div><!-- .container -->
+			</div><!-- .summary -->
+			<div id="html" class="hide">
+				<label>Embed code to your website:</label><br/>
+				<textarea id="holdtext" style="width: 50%; height: 100px;"></textarea><br/>
+			</div><!-- #html -->
+		</div><!-- .list-wrap -->
+	</div><!-- #example-two -->
+</div><!-- .grid_16 -->
